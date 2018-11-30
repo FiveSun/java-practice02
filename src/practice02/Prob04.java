b74d05a3b0061bd8fd438694a6cc009a61222796
FiveSun <FiveSun@TFG155-FIVESUN>
2018-12-01 00:15:30 +0900

    5번문제 에러

--- src/practice02/Prob04.java
+++ src/practice02/Prob04.java
@@ -1,20 +1,29 @@
 package practice02;
 
-import java.util.Scanner;
-
 public class Prob04 {
 
+//	[문제4]
+//			문자열(String)을 입력 받아, 해당 문자열을 문자 순서를 뒤집어서 char[]로 반환하는 메소드를 만들고, 
+//	char[]을 입력 받아 출력하는 메소드를 만드시오.
+
 	public static void main(String[] args) {
-		char[] arrayReverse1 = reverse("Hello World"));
+		char[] arrayReverse1 = reverse("Hello World");
 		printCharArray(arrayReverse1);
 		
-		char[] arrayReverse2 = reverse("Java Programming!"));
+		char[] arrayReverse2 = reverse("Java Programming!");
 		printCharArray(arrayReverse2);
 	}
 
 	public static char[] reverse(String  str) {
+		char[] tempchar;
+		tempchar = new char[str.length()];
+		for(int i=0; i < str.length(); i++) {
+			tempchar[str.length()-1 - i] = str.charAt(i);
+		}
+		return tempchar;
 	}
 	
 	public static void printCharArray(char[] array) {
+		System.out.println(array);
 	}
 }
--- src/practice02/Prob05.java
+++ src/practice02/Prob05.java
@@ -1,8 +1,55 @@
 package practice02;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Prob05 {
+//[문제5]
+//	숨겨진 카드의 수를 맞추는 게임입니다. 1-100까지의 임의의 수를 가진 카드를 한 장
+//	숨기고 이 카드의 수를 맞추는 게임입니다. 아래의 화면과 같이 카드 속의 수가 57인 경
+//	우를 보면 수를 맞추는 사람이 40이라고 입력하면 "더 높게", 다시 75이라고 입력하면
+//	"더 낮게" 라는 식으로 범위를 좁혀가며 수를 맞추고 있습니다. 게임을 반복하기 위해
+//	y/n이라고 묻고 n인 경우 종료됩니다..
 
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		boolean quit = true;
+		while (quit) {
+			Random r = new Random();
+			int k = r.nextInt(100) + 1;
+			System.out.println("수를 결정하였습니다. 맞추어 보세요"+k);
+			int max = 100;
+			int min = 1;
+			for (int i = 1; i < 10000; i++) {
+
+				System.out.println(min + "-" + max);
+				System.out.print(i + ">>");
+				int number = scanner.nextInt();
+				if (number < k) {
+					System.out.println("더 높게");
+					min = number;
+				} else if (number > k) {
+					System.out.println("더 낮게");
+					max = number;
+				} else {
+					System.out.print("맞았습니다.\n다시하시겠습니까(y/n)>>");
+					String answer = scanner.nextLine();
+					if (answer.equals("y")) {
+						quit = false;
+						return;
+						// break;
+					}
+				}
+
+//			String answer = scanner.nextLine();
+
+			}
+		}
+		scanner.close();
 	}
 
+	// String answer = scanner.nextLine();
+
+	// if(answer.equals("y"))
 }
